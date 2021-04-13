5a8ab0bfa809cab2871381d83dcd87f4d986b26c
KulaJunke <junkekula@gmail.com>
2021-04-13 07:59:28 +0200

    Unentschieden
    Wenn ich alles richtig gemacht habe, kann man jetzt auch unentschieden spielen.

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -54,7 +54,8 @@ public class main {
         else if(board[2] == board[4] && board[2] == board[6] && board[0] != -1) {
             winner = board[2];
         }
-        else{
+        else if(board[0] != -1 && board[1] != -1 && board[2] != -1 && board[3] != -1 && board[4] != -1 && board[5] != -1
+                && board[6] != -1 && board[7] != -1 && board[8] != -1){
             winner = -1;
         }
         return winner;
